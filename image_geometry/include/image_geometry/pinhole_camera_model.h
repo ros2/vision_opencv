1c8041a3d15f565181e2304f831dbea99a618660
Chris Lalancette <clalancette@openrobotics.org>
2017-06-06 22:30:16 +0000

    Add visibility_control for Windows compatibility.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- image_geometry/include/image_geometry/pinhole_camera_model.h
+++ image_geometry/include/image_geometry/pinhole_camera_model.h
@@ -1,6 +1,8 @@
 #ifndef IMAGE_GEOMETRY_PINHOLE_CAMERA_MODEL_H
 #define IMAGE_GEOMETRY_PINHOLE_CAMERA_MODEL_H
 
+#include "image_geometry/visibility_control.hpp"
+
 #include <sensor_msgs/msg/camera_info.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -22,31 +24,37 @@ public:
 class PinholeCameraModel
 {
 public:
-  
+  IMAGE_GEOMETRY_PUBLIC
   PinholeCameraModel();
 
+  IMAGE_GEOMETRY_PUBLIC
   PinholeCameraModel(const PinholeCameraModel& other);
 
+  IMAGE_GEOMETRY_PUBLIC
   PinholeCameraModel& operator=(const PinholeCameraModel& other);
 
   /**
    * \brief Set the camera parameters from the sensor_msgs/CameraInfo message.
    */
+  IMAGE_GEOMETRY_PUBLIC
   bool fromCameraInfo(const sensor_msgs::msg::CameraInfo& msg);
 
   /**
    * \brief Set the camera parameters from the sensor_msgs/CameraInfo message.
    */
+  IMAGE_GEOMETRY_PUBLIC
   bool fromCameraInfo(const sensor_msgs::msg::CameraInfo::ConstSharedPtr& msg);
 
   /**
    * \brief Get the name of the camera coordinate frame in tf.
    */
+  IMAGE_GEOMETRY_PUBLIC
   std::string tfFrame() const;
 
   /**
    * \brief Get the time stamp associated with this camera model.
    */
+  IMAGE_GEOMETRY_PUBLIC
   builtin_interfaces::msg::Time stamp() const;
 
   /**
@@ -55,6 +63,7 @@ public:
    * The maximum resolution at which the camera can be used with the current
    * calibration; normally this is the same as the imager resolution.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Size fullResolution() const;
 
   /**
@@ -64,24 +73,31 @@ public:
    * reduced by binning/ROI and affected by distortion. If binning and ROI are
    * not in use, this is the same as fullResolution().
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Size reducedResolution() const;
 
+  IMAGE_GEOMETRY_PUBLIC
   cv::Point2d toFullResolution(const cv::Point2d& uv_reduced) const;
 
+  IMAGE_GEOMETRY_PUBLIC
   cv::Rect toFullResolution(const cv::Rect& roi_reduced) const;
 
+  IMAGE_GEOMETRY_PUBLIC
   cv::Point2d toReducedResolution(const cv::Point2d& uv_full) const;
 
+  IMAGE_GEOMETRY_PUBLIC
   cv::Rect toReducedResolution(const cv::Rect& roi_full) const;
 
   /**
    * \brief The current raw ROI, as used for capture by the camera driver.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Rect rawRoi() const;
 
   /**
    * \brief The current rectified ROI, which best fits the raw ROI.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Rect rectifiedRoi() const;
 
   /**
@@ -92,6 +108,7 @@ public:
    * \param xyz 3d point in the camera coordinate frame
    * \return (u,v) in rectified pixel coordinates
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Point2d project3dToPixel(const cv::Point3d& xyz) const;
 
   /**
@@ -105,113 +122,135 @@ public:
    * \param uv_rect Rectified pixel coordinates
    * \return 3d ray passing through (u,v)
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Point3d projectPixelTo3dRay(const cv::Point2d& uv_rect) const;
 
   /**
    * \brief Rectify a raw camera image.
    */
+  IMAGE_GEOMETRY_PUBLIC
   void rectifyImage(const cv::Mat& raw, cv::Mat& rectified,
                     int interpolation = cv::INTER_LINEAR) const;
 
   /**
    * \brief Apply camera distortion to a rectified image.
    */
+  IMAGE_GEOMETRY_PUBLIC
   void unrectifyImage(const cv::Mat& rectified, cv::Mat& raw,
                       int interpolation = cv::INTER_LINEAR) const;
 
   /**
    * \brief Compute the rectified image coordinates of a pixel in the raw image.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Point2d rectifyPoint(const cv::Point2d& uv_raw) const;
 
   /**
    * \brief Compute the raw image coordinates of a pixel in the rectified image.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Point2d unrectifyPoint(const cv::Point2d& uv_rect) const;
 
   /**
    * \brief Compute the rectified ROI best fitting a raw ROI.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Rect rectifyRoi(const cv::Rect& roi_raw) const;
 
   /**
    * \brief Compute the raw ROI best fitting a rectified ROI.
    */
+  IMAGE_GEOMETRY_PUBLIC
   cv::Rect unrectifyRoi(const cv::Rect& roi_rect) const;
 
   /**
    * \brief Returns the camera info message held internally
    */
+  IMAGE_GEOMETRY_PUBLIC
   const sensor_msgs::msg::CameraInfo& cameraInfo() const;
 
   /**
    * \brief Returns the original camera matrix.
    */
+  IMAGE_GEOMETRY_PUBLIC
   const cv::Matx33d& intrinsicMatrix() const;
 
   /**
    * \brief Returns the distortion coefficients.
    */
+  IMAGE_GEOMETRY_PUBLIC
   const cv::Mat_<double>& distortionCoeffs() const;
 
   /**
    * \brief Returns the rotation matrix.
    */
+  IMAGE_GEOMETRY_PUBLIC
   const cv::Matx33d& rotationMatrix() const;
 
   /**
    * \brief Returns the projection matrix.
    */
+  IMAGE_GEOMETRY_PUBLIC
   const cv::Matx34d& projectionMatrix() const;
 
   /**
    * \brief Returns the original camera matrix for full resolution.
    */
+  IMAGE_GEOMETRY_PUBLIC
   const cv::Matx33d& fullIntrinsicMatrix() const;
 
   /**
    * \brief Returns the projection matrix for full resolution.
    */
+  IMAGE_GEOMETRY_PUBLIC
   const cv::Matx34d& fullProjectionMatrix() const;
 
   /**
    * \brief Returns the focal length (pixels) in x direction of the rectified image.
    */
+  IMAGE_GEOMETRY_PUBLIC
   double fx() const;
 
   /**
    * \brief Returns the focal length (pixels) in y direction of the rectified image.
    */
+  IMAGE_GEOMETRY_PUBLIC
   double fy() const;
 
   /**
    * \brief Returns the x coordinate of the optical center.
    */
+  IMAGE_GEOMETRY_PUBLIC
   double cx() const;
 
   /**
    * \brief Returns the y coordinate of the optical center.
    */
+  IMAGE_GEOMETRY_PUBLIC
   double cy() const;
 
   /**
    * \brief Returns the x-translation term of the projection matrix.
    */
+  IMAGE_GEOMETRY_PUBLIC
   double Tx() const;
 
   /**
    * \brief Returns the y-translation term of the projection matrix.
    */
+  IMAGE_GEOMETRY_PUBLIC
   double Ty() const;
 
   /**
    * \brief Returns the number of columns in each bin.
    */
+  IMAGE_GEOMETRY_PUBLIC
   uint32_t binningX() const;
 
   /**
    * \brief Returns the number of rows in each bin.
    */
+  IMAGE_GEOMETRY_PUBLIC
   uint32_t binningY() const;
   
   /**
@@ -222,6 +261,7 @@ public:
    * \param deltaX Delta X, in Cartesian space
    * \param Z      Z (depth), in Cartesian space
    */
+  IMAGE_GEOMETRY_PUBLIC
   double getDeltaU(double deltaX, double Z) const;
 
   /**
@@ -232,6 +272,7 @@ public:
    * \param deltaY Delta Y, in Cartesian space
    * \param Z      Z (depth), in Cartesian space
    */
+  IMAGE_GEOMETRY_PUBLIC
   double getDeltaV(double deltaY, double Z) const;
 
   /**
@@ -242,6 +283,7 @@ public:
    * \param deltaU Delta u, in pixels
    * \param Z      Z (depth), in Cartesian space
    */
+  IMAGE_GEOMETRY_PUBLIC
   double getDeltaX(double deltaU, double Z) const;
 
   /**
@@ -252,11 +294,13 @@ public:
    * \param deltaV Delta v, in pixels
    * \param Z      Z (depth), in Cartesian space
    */
+  IMAGE_GEOMETRY_PUBLIC
   double getDeltaY(double deltaV, double Z) const;
 
   /**
    * \brief Returns true if the camera has been initialized
    */
+  IMAGE_GEOMETRY_PUBLIC
   bool initialized() const { return (bool)cache_; }
 
 protected:
